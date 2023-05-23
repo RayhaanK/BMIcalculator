302a038893c480358586234bd28aeed58801c7e1
RayhaanK <rayhaankhan331@gmail.com>
2023-05-23 15:04:36 +0200

    added colours

--- script/code.js
+++ script/code.js
@@ -12,19 +12,25 @@ function calculation() {
 //   To make sure no invalid text is inserted or not at all
   if (height === "" || isNaN(height)) {
     display.innerHTML = "This is not a valid height";
+    display.style.color = "red"
   } else if (weight === "" || isNaN(weight)) {
     display.innerHTML = "This is not a valid weight";
+    display.style.color = "red"
   } else {
      bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //  Declaring whether you are Under Weight, Normal, Overweight or Obese
     if (bmi < 18.5) {
-            display.innerHTML.body.style.color = `You are Under Weight: ${bmi}`;
+            display.innerHTML = `You are Under Weight: ${bmi}`;
+            display.style.color = "orange"
           } else if (bmi >= 18.5 && bmi < 24.9) {
             display.innerHTML = `You are Normal Weight: ${bmi}`;
+            display.style.color = "green"
           } else if (bmi >= 25 && bmi < 29.9) {
             display.innerHTML = `You are Overweight: ${bmi}`;
-          } else {
+            display.style.color = "#990000"
+        } else {
             display.innerHTML = `You are Obese: ${bmi}`;
+            display.style.color = "#F94A29"
     }
   }}
 
